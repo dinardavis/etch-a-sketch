b3d6a1c73cf84980f873227c63397ea812b25fdb
Dinar Davis <dinardavis@gmail.com>
2022-06-11 16:43:59 +0700

    Add createGrid() to add div grid to DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert("hello!");
+const gridContainer = document.querySelector(".grid-container");
+
+
+function createGrid() {
+  for (let i = 0; i < 16; i++) {
+    gridContainer.innerHTML += '<div class="grid-squares"></div>';
+  }
+}
+
+createGrid();
