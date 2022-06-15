1cc35d58270a54462c997fd099336bddf2a0a337
Dinar Davis <dinardavis@gmail.com>
2022-06-15 17:44:38 +0700

    Add styling to user interface

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector(".grid");
 const gridOverlay = document.querySelector(".grid-overlay");
+const randomColorBtn = document.querySelector(".random-color-btn");
 let slider = document.getElementById("slider");
 let output = document.getElementById("slider-value");
 output.innerText = `Grid Size: ${slider.value} x ${slider.value}`;
@@ -23,8 +24,19 @@ function updateSliderValue() {
 const gridSquares = document.querySelectorAll('.grid-squares');
 const colorPicker = document.querySelector(".color-picker");
 
+
+
 function changeSquareColor(e) {
   e.target.style.backgroundColor = `${colorPicker.value}`;
+}
+
+function randomColor() {
+  var letters = '0123456789ABCDEF';
+  var randomColorHex = '#';
+  for (var i = 0; i < 6; i++) {
+    randomColorHex += letters[Math.floor(Math.random() * 16)];
+  }
+  e.target.style.backgroundColor = `${colorPicker.randomColorHex}`;;
 }
 
 function toggleGridOverlay() {
